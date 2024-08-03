49bf405242c314298548ca11640b3353be81c4fe
eric-armendariz <earmen3@illinois.edu>
2024-08-03 16:45:01 -0500

    implemented vc log

--- vc/base.py
+++ vc/base.py
@@ -1,5 +1,8 @@
+import itertools
+import operator
 import os
 
+from collections import namedtuple
 from . import data
 
 def write_tree(directory='.'):
@@ -93,3 +96,22 @@ def commit(message):
     data.set_HEAD(oid)
     
     return oid
+
+Commit = namedtuple('Commit', ['tree', 'parent', 'message'])
+
+def get_commit(oid):
+    parent = None
+    
+    commit = data.get_object(oid, 'commit').decode()
+    lines = iter(commit.splitlines())
+    for line in itertools.takewhile(operator.truth, lines):
+        key, value = line.split(' ', 1)
+        if key == 'tree':
+            tree = value
+        elif key == 'parent':
+            parent = value
+        else:
+            assert False, f'Unknown field {key}'
+    
+    message = '\n'.join(lines)
+    return Commit(tree, parent, message)
