f5c41e8a65189a9d5911e166c09b7b2695ec085a
eric-armendariz <earmen3@illinois.edu>
2024-08-04 18:06:15 -0500

    Implemented tags to reference branches

--- vc/base.py
+++ vc/base.py
@@ -1,6 +1,7 @@
 import itertools
 import operator
 import os
+import string
 
 from collections import namedtuple
 from . import data
@@ -84,7 +85,7 @@ def _empty_current_directory():
 def commit(message):
     commit = f'tree {write_tree()}\n'
     
-    HEAD = data.get_HEAD()
+    HEAD = data.get_ref('HEAD')
     if HEAD:
         commit += f'parent {HEAD}\n'
         
@@ -93,7 +94,7 @@ def commit(message):
     
     oid = data.hash_object(commit.encode(), 'commit')
     
-    data.set_HEAD(oid)
+    data.update_ref('HEAD', oid)
     
     return oid
 
@@ -119,4 +120,28 @@ def get_commit(oid):
 def checkout(oid):
     commit = get_commit(oid)
     read_tree(commit.tree)
-    data.set_HEAD(oid)
+    data.update_ref('HEAD', oid)
+    
+def create_tag(name, oid):
+    data.update_ref(f'refs/tags/{name}', oid)
+    
+def get_oid(name):
+    if name == '@' : name = 'HEAD'
+    
+    #Name is ref
+    refs_to_try = [
+        f'{name}',
+        f'refs/{name}',
+        f'refs/tags/{name}',
+        f'refs/heads/{name}',
+    ]
+    for ref in refs_to_try:
+        if data.get_ref(ref):
+            return data.get_ref(ref)
+        
+    #Name is SHA1
+    is_hex = all(c in string.hexdigits for c in name)
+    if len(name) == 40 and is_hex:
+        return name
+    
+    assert False, f'Unknown name {name}'
